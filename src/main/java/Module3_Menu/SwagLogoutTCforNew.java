a537909047a767b34c9df508e447392cc6468564
NirajAth <ninjajod619@gmail.com>
2023-07-08 22:06:15 +0530

    Added new File Jai Shree Ram... Jai Shree Hanuman...

---
+++ src/main/java/Module3_Menu/SwagLogoutTCforNew.java
@@ -0,0 +1,14 @@
+package Module3_Menu;
+
+public class SwagLogoutTCforNew {
+
+	public static void main(String[] args) {
+		
+		
+		
+		System.out.println("Jai Shree Ram....");
+		System.out.println("Jai Shree Hanuman...");
+	}
+	
+	
+}
